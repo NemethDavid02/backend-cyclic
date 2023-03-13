e5bbd6f9331e53ecf764ec9904d86d4c2467dcc3
takacs.david <takacs.david@C202-13>
2023-03-13 08:31:30 +0100

    import payment

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import PaymentController from "payment/payment.controller";
+
 import App from "./app";
 import AuthenticationController from "./authentication/authentication.controller";
 // import PostController from "./post/post.controller";
@@ -7,11 +9,11 @@ import OrderController from "./order/order.controller";
 import OrderDetailController from "./orderdetail/orderdetail.controller";
 import ProductController from "./product/product.Controller";
 import UserController from "./user/user.controller";
-
 new App([
     new AuthenticationController(),
     new UserController(),
     new OrderController(),
     new ProductController(),
     new OrderDetailController(),
+    new PaymentController(),
 ]);
