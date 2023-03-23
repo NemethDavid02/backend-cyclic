f029e507e2e1403d57d049fe46ce124c00549f28
takacs.david <takacs.david@C202-13>
2023-03-23 13:29:22 +0100

    zip changes

--- src/user/shippingAddress.dto.ts
+++ src/user/shippingAddress.dto.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { IsString, IsInt } from "class-validator";
+import { IsInt, IsString } from "class-validator";
 
 import IShippingAddress from "./shippingAddress.interface";
 
