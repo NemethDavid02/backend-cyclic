1d977577b5e9ae16b03899092ccedbf3d8ea0a85
takacs.david <takacs.david@C202-13>
2023-04-24 08:09:28 +0200

    unit test working

--- src/order/_tests_/order.test.ts
+++ src/order/_tests_/order.test.ts
@@ -1,15 +1,21 @@
 import "dotenv/config";
 
-import OrderController from "../order.controller";
-import AuthenticationController from "../../authentication/authentication.controller";
 import request from "supertest";
 
 import App from "../../app";
+import AuthenticationController from "../../authentication/authentication.controller";
+import OrderController from "../order.controller";
 
 let server: Express.Application;
+let cookie: string | any;
 
 beforeAll(async () => {
     server = new App([new OrderController(), new AuthenticationController()]).getServer();
+    const response = await request(server).post("/auth/login").send({
+        email: "masni6431@gmail.com",
+        password: "Poppyflovers",
+    });
+    cookie = response.headers["set-cookie"];
 });
 
 describe("test API endpoints", () => {
@@ -22,39 +28,21 @@ describe("test API endpoints", () => {
         expect(response.statusCode).toEqual(401);
     });
 
-    it("GET /auth/login", async () => {
-        const response = await request(server).post("/auth/login").send({
-            email: "masni6431@gmail.com",
-            password: "Poppyflovers",
-        });
+    it("GET /orders/639757ec0c5330c7405f764d", async () => {
+        const response = await request(server).get("/orders/639757ec0c5330c7405f764d").set("Cookie", cookie);
         expect(response.statusCode).toEqual(200);
-        expect(response.body._id).toEqual("63fc807e3f944579a10703c9");
-        expect(response.body.address.city).toEqual("Szalmaharmad");
-        expect(response.body.address.country).toEqual("Hungary");
-        expect(response.body.address.street).toEqual("35 Spirál u.");
-        expect(response.body.address.zip).toEqual(9464);
-        expect(response.body.shippingAddress.city).toEqual("Szalmaharmad");
-        expect(response.body.shippingAddress.country).toEqual("Hungary");
-        expect(response.body.shippingAddress.street).toEqual("35 Spirál u.");
-        expect(response.body.shippingAddress.zip).toEqual(9464);
-        expect(response.body.email).toEqual("masni6431@gmail.com");
-        expect(response.body.firstName).toEqual("Barna");
-        expect(response.body.lastName).toEqual("Orsolya");
-    });
-
-    it("GET /orders", async () => {
-        const response = await request(server).get("/orders/639757ec0c5330c7405f764d");
-        expect(response.statusCode).toEqual(200);
-        expect(response.body.userId).toEqual("63fc807e3f944579a10703b9");
+        expect(response.body._id).toEqual("639757ec0c5330c7405f764d");
+        expect(response.body.userId._id).toEqual("63fc807e3f944579a10703b9");
         expect(response.body.paymentStatus).toEqual("processed");
-        expect(response.body.shippingAddress.country).toEqual("Zedónia");
-        expect(response.body.shippingAddress.street).toEqual("154 Körkör u.");
-        expect(response.body.shippingAddress.city).toEqual("Villámkert");
-        expect(response.body.shippingAddress.zip).toEqual("43122");
+        expect(response.body.status).toEqual("shipped");
+        expect(response.body.shippingAdress.country).toEqual("Zedónia");
+        expect(response.body.shippingAdress.street).toEqual("154 Körkör u.");
+        expect(response.body.shippingAdress.city).toEqual("Villámkert");
+        expect(response.body.shippingAdress.zip).toEqual("43122");
         expect(response.body.billingAddress.country).toEqual("Zedónia");
         expect(response.body.billingAddress.street).toEqual("154 Körkör u.");
         expect(response.body.billingAddress.city).toEqual("Villámkert");
-        expect(response.body.billingAddress.zip).toEqual("43122");
+        expect(response.body.billingAddress.zip).toEqual(43122);
     });
 
     it("GET /auth/logout", async () => {
